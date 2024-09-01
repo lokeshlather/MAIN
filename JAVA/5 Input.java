9ec64ad782b3339e64d090bf41a01f8590fc128d
lokeshlather <161685710+lokeshlather@users.noreply.github.com>
2024-09-01 21:20:35 +0530

    .

---
+++ JAVA/5 Input.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class input {
+    public static void main(String args[]){
+        System.out.print("Enter Name: ");
+        Scanner sc = new Scanner(System.in);
+        String Name = sc.nextLine();
+        System.out.println(Name);
+        
+    }
+    
+}
