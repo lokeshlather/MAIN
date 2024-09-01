eacc8e15a0a8375144cdc7f267465829694cc31b
lokeshlather <161685710+lokeshlather@users.noreply.github.com>
2024-09-01 21:31:15 +0530

    .

---
+++ JAVA/12  DoWhileLoop.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String args[]){
+
+        // System.out.print("Enter Number: ");
+        // Scanner sc = new Scanner(System.in);
+        // int num = sc.nextInt();
+        int i=0;
+
+        do{
+            i++;
+            System.out.println(i);
+        }while(i!=1);//>num instead of !=1 if user input is taken
+    }
+    
+}
