199335096ace6e5153e42d324b7fd27fe9766cad
lokeshlather <161685710+lokeshlather@users.noreply.github.com>
2024-09-01 20:03:17 +0530

    Add files via upload

---
+++ JAVA/Variables.java
@@ -0,0 +1,18 @@
+public class Variables {
+    public static void main(String args[]){
+        /*Integer Variables*/
+        int a=10;
+        int b=20;
+        System.out.println(a);
+        /*Changing Value of a*/
+        a=15;
+        System.out.println(a);
+
+        /*String Variables*/
+        String name="Lokesh Lather";
+        System.out.println(name);
+        /*Changing Value of name*/
+        name="Lokesh";
+        System.out.println(name);  
+    }
+}
